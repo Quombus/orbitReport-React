c13ebc870386fe0c9b496ac9395905c378281f7f
Quombus <beckettrichardmatthew@gmail.com>
2024-08-25 10:48:05 -0500

    commit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,27 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
+  
   return (
-    <table>
+  
+  <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((id) =>
+      <tr key={id}>
+      <td>{id.name}</td>
+      <td>{id.type}</td>
+      <td>{id.launchDate}</td>
+      <td>{id.operational? "Active" : "Inactive"}</td>
+    </tr>
+  )}
      </tbody>
    </table>
   );
